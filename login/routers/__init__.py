16cd8f890b1cfa44423bf694e23ea44ca953701d
dsj-1004 <soppl1004@gmail.com>
2026-02-01 01:36:42 +0900

    feat: init.py

---
+++ login/routers/__init__.py
@@ -0,0 +1,7 @@
+from .auth import router as auth_router
+from .family import router as family_router
+from .hospitals import router as hospitals_router
+from .profiles import router as profiles_router
+from .users import router as users_router
+
+__all__ = ["auth_router", "users_router", "profiles_router", "hospitals_router", "family_router"]
